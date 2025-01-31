62aeeea1b6992b9eae6afd0ecd1752d18830710f
jayelmeynak <magomed110301@yandex.ru>
2025-01-31 21:42:13 +0300

    move some files

--- app/src/main/java/com/example/cryptocurrency/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.cryptocurrency
+package com.example.cryptocurrency.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.cryptocurrency.ui.theme.CryptoCurrencyTheme
+import com.example.cryptocurrency.presentation.ui.theme.CryptoCurrencyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
