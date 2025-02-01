b3fd1f01290d08e87c854608137b63ca3a7adb06
jayelmeynak <magomed110301@yandex.ru>
2025-02-01 05:32:08 +0300

    feat(ui): добавление базовой дизайн-системы приложения
    - Определена цветовая палитра
    - Создан Shapes
    - Реализована типографика:
      * 6 текстовых стилей (displayLarge, titleMedium и др.)
      * Вариации размеров 14-30sp и весов шрифтов
    - Настроены цветовые схемы
    - Интеграция с Material Design 3

--- app/src/main/java/com/example/cryptocurrency/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/cryptocurrency/presentation/ui/theme/Type.kt
@@ -6,29 +6,31 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+    ),
+    displayMedium = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    titleLarge = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyMedium = TextStyle(
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
