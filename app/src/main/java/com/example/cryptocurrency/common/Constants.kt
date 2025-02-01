b72b2050fb078306114144fd4ee417f441a91e61
jayelmeynak <magomed110301@yandex.ru>
2025-02-01 07:55:11 +0300

    feat(common): Добавил константы для навигации

--- app/src/main/java/com/example/cryptocurrency/common/Constants.kt
+++ app/src/main/java/com/example/cryptocurrency/common/Constants.kt
@@ -2,4 +2,7 @@ package com.example.cryptocurrency.common
 
 object Constants {
     const val BASE_URL = "https://api.coinpaprika.com/"
+    const val COIN_LIST_ROUTE = "coin_list_screen"
+    const val COIN_DETAIL_ROUTE = "coin_detail_screen"
+    const val PARAM_COIN_ID = "coinId"
 }
